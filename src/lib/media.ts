e66766dd26c678ac32200073589d8c0e06bf4309
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-12 17:52:20 +0900

    perf: optimize cinematic hero media delivery

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -1,6 +1,6 @@
 export const media = {
   heroStill: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_51451d0d-3297-4adb-bd24-cd036d3736a5_min.webp',
-  heroVideo: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083534_67154093-b517-48bb-994d-75ef388c8730.mp4',
+  heroVideo: 'https://d2ol7oe51mr4n9.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/a44a675a-1ede-45b7-84b8-b78ae28a7644.mp4',
   systems: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_a7db00d7-767f-4179-b75b-0d18e345e3b7_min.webp',
   builders: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_225b24ba-13c4-4d3f-8fc2-eb3277e00cab_min.webp',
   global: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_a652e5a5-7adb-4668-99b2-0dcdd2cee107_min.webp',
