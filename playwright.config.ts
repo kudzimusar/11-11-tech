8987d7e0a0f8c1b192b238add352f0d8db17e182
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-12 16:44:48 +0900

    test: run phone certification on Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     timeout: 30_000,
   },
   projects: [
-    { name: 'desktop-chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'mobile-chromium', use: { ...devices['iPhone 13'] } },
+    { name: 'desktop-chromium', use: { ...devices['Desktop Chrome'], browserName: 'chromium' } },
+    { name: 'mobile-chromium', use: { ...devices['Pixel 7'], browserName: 'chromium' } },
   ],
 })
