1c3e9b93c91ed1ce3f47f208d3f650afedec86d7
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-12 16:45:56 +0900

    routing: fold Pages recovery into application entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
+const location = window.location
+if (location.search[1] === '/') {
+  const decodedRoute = location.search
+    .slice(1)
+    .split('&')
+    .map((segment) => segment.replace(/~and~/g, '&'))
+    .join('?')
+  window.history.replaceState(null, '', `${location.pathname.slice(0, -1)}${decodedRoute}${location.hash}`)
+}
+
 document.documentElement.classList.add('js')
 
 createRoot(document.getElementById('root')!).render(
