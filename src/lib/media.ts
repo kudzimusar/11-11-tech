23b0b68823a5c87f5fbb15fbc591c32fbe697c5b
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-13 05:57:29 +0900

    feat: add 2.2 editorial visual asset library

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -5,4 +5,45 @@ export const media = {
   builders: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_225b24ba-13c4-4d3f-8fc2-eb3277e00cab_min.webp',
   global: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_a652e5a5-7adb-4668-99b2-0dcdd2cee107_min.webp',
   intelligence: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_083500_2eaf1612-6cbe-42df-9ff7-90ea2ce9e901_min.webp',
+
+  crossCulture: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_f248a901-4953-41c5-af1a-e01203417856_min.webp',
+  tokyoBusiness: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_40310c7e-951a-4cc6-90fa-25b95ae6f108_min.webp',
+  harareBusiness: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_4ba5b1af-fea1-43e4-b905-8bda09dc2c59_min.webp',
+  uiUxEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_29afbae7-378f-4133-83b1-4f80a24486e9_min.webp',
+  crmEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_11023afe-3ee9-4fc6-82ad-2b8feb6909e6_min.webp',
+  aiEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205415_3e9bb92d-a5ff-473f-b993-9cc8fe0ffb63_min.webp',
+  cloudEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_1c3dd8ef-2f80-471f-8e45-c336b98c2f7e_min.webp',
+  transformationEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_c51555b2-a25a-4b89-8b01-bec7aeb9d3d4_min.webp',
+  talentEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_354b376a-9bd2-487b-8a50-64b226d9c1ad_min.webp',
+  trustEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_97860301-c2d9-476a-a6a8-08cfcb955dce_min.webp',
+
+  automotiveEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_3b0c5a8d-b89f-40ce-b6f0-c21724d4f03e_min.webp',
+  mediaEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205445_1073b715-10d4-46a9-a4da-509df4af3704_min.webp',
+  educationEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_ee5a1f9c-3fa6-4b04-a392-e402e515d02a_min.webp',
+  commerceEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_be717a27-b2af-4594-bb63-990d4dcbb53f_min.webp',
+  propertyEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_c77c4186-b975-4349-9dda-35b6afd2a449_min.webp',
+  nonprofitEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_930948be-2ebb-4576-9527-2c62e28c5826_min.webp',
+  abstractEditorial: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_ce998f83-9468-41c0-a546-107be3335a44_min.webp',
+  globalBridge: 'https://d8j0ntlcm91z4.cloudfront.net/user_3INtVVsrCBipnAp2zgCDZq6LsaM/hf_20260912_205523_5ecf8d73-46d7-4c12-b1e8-9a045677b32b_min.webp',
+} as const
+
+export const capabilityMedia = {
+  'ui-ux': media.uiUxEditorial,
+  enterprise: media.crmEditorial,
+  ai: media.aiEditorial,
+  'software-data-cloud': media.cloudEditorial,
+  transformation: media.transformationEditorial,
+  talent: media.talentEditorial,
+  trust: media.trustEditorial,
+} as const
+
+export const industryMedia = {
+  automotive: media.automotiveEditorial,
+  nonprofit: media.nonprofitEditorial,
+  media: media.mediaEditorial,
+  education: media.educationEditorial,
+  commerce: media.commerceEditorial,
+  property: media.propertyEditorial,
+  recruitment: media.talentEditorial,
+  institutional: media.trustEditorial,
 } as const
