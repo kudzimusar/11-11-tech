b16745009dcae0d9bcea5c0a1b1d95a503bfd5fc
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-12 16:46:32 +0900

    routing: remove obsolete recovery asset

--- public/route-recovery.js
+++
@@ -1,9 +0,0 @@
-(function recoverGitHubPagesRoute(location) {
-  if (location.search[1] !== '/') return
-  const decoded = location.search
-    .slice(1)
-    .split('&')
-    .map((segment) => segment.replace(/~and~/g, '&'))
-    .join('?')
-  window.history.replaceState(null, '', location.pathname.slice(0, -1) + decoded + location.hash)
-})(window.location)
